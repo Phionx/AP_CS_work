47e2fc8bf3344b7fecd67161df33d13d44b0c5d4
Phionx <shanjha.usa@gmail.com>
2015-11-30 18:47:02 -0500

    bashmoney

--- HW/38/GuessNumber.java
+++ HW/38/GuessNumber.java
@@ -5,6 +5,33 @@ HW38--Keep Guessing
 2015-11-30
 */
 
-public class GuessNumber {
+import cs1.Keyboard;
 
+public class GuessNumber {
+private int guesses;
+//Tells if high or low
+public boolean teller(int target){
+int num = 0;
+while(num >
+if(target > num){
+System.out.println("Too low, try again...");
+guesses++;
+return false;
+} else if(target < num){
+System.out.println("Too high, try again...");
+guesses++;
+return false;
+} else if(target == num){
+System.out.println("Correct! It took " + guesses + " guesses.");
+return true;
+}
+
+public static void main(String [] args){
+int number = (int)(Math.random()*100) + 1;
+boolean state = false;
+
+while(!state){
+
+}
+}
 }
