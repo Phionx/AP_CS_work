0b6c561a5616a88cda167e0e3f97441befeb0d00
Phionx <shanjha.usa@gmail.com>
2015-12-06 23:04:50 -0500

    42

---
+++ HW/42/ListInt.java
@@ -0,0 +1,19 @@
+public interface ListInt {
+
+    void add( int newVal );
+    //no return
+    //adds newVal to beginning of array
+    //right justified
+    
+    void addAtIndex( int newVal, int index );
+    //adds newVal at index, shifting errythang to the right
+    //fails for index >= size
+    
+    void remove( int index );
+    //removes value at index, shifting everything in front to the left
+    
+    int size();
+    //returns the number of meaningful values in the array
+    //one more than _lastPos
+    
+}
