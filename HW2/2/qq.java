2ffba36cf646ee22f84477faf3e3e556ef891b3a
Phionx <shanjha.usa@gmail.com>
2016-02-25 02:30:09 -0500

    fact

---
+++ HW2/2/qq.java
@@ -0,0 +1,16 @@
+/*
+Shantanu Jha
+APCS1 pd5
+HW2--Exploration
+2016-02-22
+*/
+
+/*
+ * a) Not possible
+ * b) Not possible
+ * c) Possible - 2 Solutions
+ * d) Possible - 3 Solutions
+ * e) Possible - 3 Solutions
+ * f) Possible - 3 Solutions
+ * g) Possible - 3 Solutions
+ */
